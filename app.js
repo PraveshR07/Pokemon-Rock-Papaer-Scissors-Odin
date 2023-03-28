f1403040b6f6a769cd246cbeb0a41edfc3af8423
Pravesh <110791976+PraveshR07@users.noreply.github.com>
2023-03-28 11:28:51 -0400

    swtich statemnt fixed with ()

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Create your parametters 
 
 let userScore = 0; 
-let computerScore  = 0; 
-const userScore_span =document.getElementById('user-score'); 
-const computerScore_span = document.getElementById('computer-score');
+let computerScore = 0; 
+const userScore_span = document.getElementById("user-score"); 
+const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board")
 const result_p = document.querySelector(".result > p"); 
 // store the variables for later usage. 
@@ -21,9 +21,11 @@ function getComputerChoice (){
     const choices = ['g', 'f', 'w']; 
     //Use floor to create whole number math.random.
     // Times random to get a number between [0-2.9999] > 3 
-    const randomNumber = (Math.floor(Math.random() * 3 ));
+    const randomNumber = Math.floor(Math.random() * 3 );
     return choices[randomNumber];
 
+}
+//Create conver to words fucntion
     function convertToWord(letter) {
         if (letter === "g") return "Grass"; 
         if (letter === "f") return "Fire"; 
@@ -31,50 +33,44 @@ function getComputerChoice (){
 
     }
 
-    
-};
-
 //Create a fucntion for the test for a win
 
 function win (userChoice, computerChoice) {
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore; 
-    const smallUserWord = "user"<font(3)
-    result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}(comp). You Win`; 
+    result_p.innerHTML = `${convertToWord(userChoice)}(user) beats ${convertToWord(computerChoice)}(comp). You Win`; 
 }
 
-function lost () {
+// Create fucntion for lose
+function lost (userChoice, computerChoice) {
+    
 
 }
 
-function draw {
+function draw (userChoice,computerChoice) {
     console.log("DRAW")
-};
-``
+} 
 function game(userChoice) {   
-    const computerChoice = getComputerChoice;
-
-    //Create a swtich statment based on the events listed on RPS. 
-    //Swtich statement 
+    const computerChoice = getComputerChoice();
     switch(userChoice + computerChoice) {
         //Create case where you win 
         case 'gw':
         case 'fg':
         case 'wf': 
-        win();
+        win(userChoice,computerChoice);
         break; 
         //Case where user loses.
         case 'wg':
         case 'gf':
         case 'fw':
-        lose();
+        lose(userChoice, computerChoice);
         break 
         //Tie
         case 'ww':
         case 'ff':
         case 'gg': 
-        tie(); 
+        tie(userChoice, compu); 
         break; 
 
     }
